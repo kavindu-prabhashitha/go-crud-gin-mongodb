e3c77fd65cd4280c51dd7824e2528e63910f0874
kavindu <kavindup@99x.io>
2023-09-11 14:45:29 +0530

    project initialized

--- main.go
+++ main.go
@@ -1,6 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kavindu-prabhashitha/go-crud-gin-mongodb/config"
+	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func init() {
+
+	var (
+		server      *gin.Engine
+		ctx         context.Context
+		mongoclient *mongo.Client
+		redisclient *redis.Client
+	)
+
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Counld not load environment bariables", err)
+	}
+
+	// ? Create a context
+	ctx = context.TODO()
+
+	// ? Connect to MongoDB
+	mongoconn := options.Client().ApplyURI(config.DBUri)
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := mongoclient.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("MongoDB successfully connected...")
+
+	// ? Connect to Redis
+	redisclient = redis.NewClient(&redis.Options{
+		Addr: config.RedisUri,
+	})
+	if _, err := redisclient.Ping(ctx).Result(); err != nil {
+		panic(err)
+	}
+	err = redisclient.Set(ctx, "test", "Welcome to Golang with Redis and MongoDB",
+		0).Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Redis client connected successfully...")
+
+	// ? Create the Gin Engine instance
+	server = gin.Default()
+
+	fmt.Print(config)
+}
 
 func main() {
 	fmt.Print("This is the main.go file")
---
+++ services/post.service.go
@@ -0,0 +1,9 @@
+package services
+
+// type PostService interface {
+// 	CreatePost(*models.CreatePostRequest) (*models.DBPost, error)
+// 	UpdatePost(string, *models.UpdatePost) (*models.DBPost, error)
+// 	FindPostById(string) (*models.DBPost, error)
+// 	FindPosts(page int, limit int) ([]*models.DBPost, error)
+// 	DeletePost(string) error
+// }
